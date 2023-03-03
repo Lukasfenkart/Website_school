f89246fcc2e774b4c5bda130be0522137bc5208e
Lukasfenkart <90911645+Lukasfenkart@users.noreply.github.com>
2023-03-03 15:21:21 +0100

    Variablen

---
+++ JS/01_Variablen_Operationen/script.js
@@ -0,0 +1,25 @@
+console.log("Hallo");
+console.log("Tolle Sache");
+
+//Variablen
+let a = 7;
+let b = 8;
+var c = 10;
+console.log("Variable a hat den Wert " + a);
+console.log("Variable b hat den Wert " + b)
+result = a + b;
+
+console.log("Variable  c = a + b hat den Wert " + c);
+result = b - a;
+console.log("Variable c = b - a hat den Wert " + c);
+result = a + b + c;
+console.log("a + b + c = " + result);
+result = (a + b) / c;
+console.log("(a + b) / c = " + result);
+result = c - a + b
+console.log("c -a + b = " + result);
+result = a + b + c * 3;
+console.log("a + b + c*3 = " + result);
+result = (a + b + c) / 4;
+console.log("(a + b + c) / 4 = " + result);
+
