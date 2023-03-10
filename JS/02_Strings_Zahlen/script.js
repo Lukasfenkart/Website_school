9b8961ed3498ef880ed87bd5bc42d97510ec01f7
Lukasfenkart <90911645+Lukasfenkart@users.noreply.github.com>
2023-03-10 14:24:26 +0100

    js 2 und 3

---
+++ JS/02_Strings_Zahlen/script.js
@@ -0,0 +1,28 @@
+console.log("Hallo");
+//komma zahlen
+let c = 7.4;
+let d = 7.5; 
+
+result = c + d;
+console.log(result);
+
+//texte
+
+let name = "Lukas";
+console.log(name);
+
+console.log("das ergebnis von " + c + " und " + d + " ist " + result);
+
+//1
+
+let a = 17.4;
+let b = 3.9;
+
+ergebnis = a / b;
+
+console.log("the result of "+a+" dividet by "+b+" is "+ergebnis);
+
+let firstname = "Lukas"
+let lastname = "Fenkart"
+
+console.log("Ich heiße "+firstname+" "+lastname)
---
+++ JS/03_Aufgaben_Variablen/script.js
@@ -0,0 +1,20 @@
+let banana = "banana"
+let apple = "apple"
+
+let bananaPricePerKilo = 2.14;
+let applePricePerKilo = 3.43;
+
+let bananaweight = 0.34;
+let appleweight = 0.22;
+
+bananaPrice = (bananaPricePerKilo * bananaweight) / 1;
+applePrice = applePricePerKilo * appleweight;
+
+console.log("banana price = "+bananaPrice)
+console.log("apple price = "+applePrice)
+
+bananaOneKg = 1 / bananaweight;
+appleOneKg = 1 / appleweight;
+
+console.log("bananas in 1kg = "+bananaOneKg)
+console.log("apple in 1kg = "+appleOneKg)
