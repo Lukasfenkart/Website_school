83fbb58ba9d014af488f95e922b98f3fa0c8f5be
Lukasfenkart <90911645+Lukasfenkart@users.noreply.github.com>
2023-03-10 15:02:36 +0100

    js 4 und 5 + css changes

---
+++ JS/04_Schleifen/script.js
@@ -0,0 +1,28 @@
+
+for(let count = 7 ;count<=45; count++){
+    console.log(count + " lukas")
+}
+console.log(" ")
+console.log("fertig")
+console.log(" ")
+
+for(let count = 9 ;count<=99; count++){
+    console.log(count)
+}
+console.log(" ")
+console.log("fertig")
+console.log(" ")
+
+for(let count = 10 ;count>=0; count--){
+    console.log(count)
+}
+console.log(" ")
+console.log("fertig")
+console.log(" ")
+
+for(let count = 1234 ;count>=1207; count--){
+    console.log(count)
+}
+console.log(" ")
+console.log("fertig")
+console.log(" ")
---
+++ JS/05_If-Bedingungen/script.js
@@ -0,0 +1,23 @@
+number = Math.floor(Math.random() * 101);
+
+if(number < 20){
+ console.log("Mini")
+}
+else if(number > 20 && number < 50){
+    console.log("Medium")   
+}
+else{
+    console.log("Large")
+}
+
+number2 = Math.floor(Math.random() * 101);
+
+if(number2 <= 7){
+    console.log("Mini")
+}
+else if(number2 > 7 && number2 < 90){
+    console.log("Medium")   
+}
+else{
+    console.log("Large")
+}
