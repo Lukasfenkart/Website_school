f3796ceab2a31644cb4fc065f02153788a26997c
Lukasfenkart <90911645+Lukasfenkart@users.noreply.github.com>
2023-03-24 14:50:23 +0100

    html js 01 und 02

---
+++ 03_HTML_CSS/01_Zugriff_JS_HTML/script.js
@@ -0,0 +1,3 @@
+let element = document.getElementById("content");
+element.innerHTML = "Tolle sache";
+console.log(element)
---
+++ 03_HTML_CSS/02_Zufallsgenerator/script.js
@@ -0,0 +1,8 @@
+function buttonClicked(){
+    console.log("clicken")
+    let num1 = document.getElementById("number1").value;
+    let num2 = document.getElementById("number2").value;
+
+    let result = parseFloat(num1) + parseFloat(num2);
+    console.log(result)
+}
