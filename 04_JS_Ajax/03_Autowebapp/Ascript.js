9a7fab36b767e23775bd7c93a3603be83668b0e1
Lukasfenkart <90911645+Lukasfenkart@users.noreply.github.com>
2023-04-13 10:11:09 +0200

    end

--- 04_JS_Ajax/03_Autowebapp/Ascript.js
+++ 04_JS_Ajax/03_Autowebapp/Ascript.js
@@ -2,8 +2,8 @@
 let urlauto1 = "https://api.api-ninjas.com/v1/cars?limit=1&make=porsche";
 let urlauto2 = "https://api.api-ninjas.com/v1/cars?limit=1&make=lamborghini";
 let urlauto3 = "https://api.api-ninjas.com/v1/cars?limit=1&make=acura";
+let urlauto4 = "https://api.api-ninjas.com/v1/cars?limit=1&model=e63";
 
-//auto toyta
 fetch(urlauto1,{
     method:"GET",    
     headers: {
@@ -20,11 +20,13 @@ fetch(urlauto1,{
         html += "<div> Baujahr: " + element.year + "</div>";
         html += "<div> Verbrauch: " + element.city_mpg + " MPG</div>";
         html += "<div> Antriebsart: " + element.drive + "</div>";
+        html += "<div> Zylinder: " + element.cylinders + "</div>";
+        html += "<div> Hubraum: " + element.displacement + "L</div>";
     });
     document.getElementById("autos1").innerHTML = html;
 })
 
-//auto focus
+
 fetch(urlauto2,{
     method:"GET",    
     headers: {
@@ -40,12 +42,14 @@ fetch(urlauto2,{
         html += "<div> Modell:" + element.model + "</div><hr>";
         html += "<div> Baujahr: " + element.year + "</div>";
         html += "<div> Verbrauch: " + element.city_mpg + " MPG</div>";
-        html += "<div> Antriebsart: " + element.drive + "</div>";
+        html += "<div> Antriebsart: " + element.drive + "</div>"; 
+        html += "<div> Zylinder: " + element.cylinders + "</div>";
+        html += "<div> Hubraum: " + element.displacement + "L</div>";
     });
     document.getElementById("autos2").innerHTML = html;
 })
 
-//auto golf
+
 fetch(urlauto3,{
     method:"GET",    
     headers: {
@@ -62,7 +66,31 @@ fetch(urlauto3,{
         html += "<div> Baujahr: " + element.year + "</div>";
         html += "<div> Verbrauch: " + element.city_mpg + " MPG</div>";
         html += "<div> Antriebsart: " + element.drive + "</div>";
+        html += "<div> Zylinder: " + element.cylinders + "</div>";
+        html += "<div> Hubraum: " + element.displacement + "L</div>";
     });
     document.getElementById("autos3").innerHTML = html;
+})
+
+fetch(urlauto4,{
+    method:"GET",    
+    headers: {
+        "X-Api-Key": "MIxpBThmrzhgTwo0r9XWQw==unSaztTA6EPsUPxH"
+    }
+})
+.then(response => response.json())
+.then(function (data){
+    console.log(data);
+    let html = "";
+    data.forEach(element => {
+        html += "<div> Marke: " + element.make + "</div>";
+        html += "<div> Modell:" + element.model + "</div><hr>";
+        html += "<div> Baujahr: " + element.year + "</div>";
+        html += "<div> Verbrauch: " + element.city_mpg + " MPG</div>";
+        html += "<div> Antriebsart: " + element.drive + "</div>";
+        html += "<div> Zylinder: " + element.cylinders + "</div>";
+        html += "<div> Hubraum: " + element.displacement + "L</div>";
+    });
+    document.getElementById("autos4").innerHTML = html;
 })
 
